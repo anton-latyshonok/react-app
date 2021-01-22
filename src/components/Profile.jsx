73b47837e07a477d204f385e69314c4b8961662b
Anton Latyshonok <antonlatyshonok@gmail.com>
2021-01-22 12:07:34 +0300

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+      <img src='https://earthsky.org/upl/2018/06/ocean-apr27-2020-Cidy-Chai-North-Pacific-scaled-e1591491800783.jpeg'></img>
+    </div>
+    <div>
+      ava + decsription
+    </div>
+    <div>
+      My posts
+      <div>
+        My post
+      </div>
+      <div>
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
+    </div>
+
+  </div>
+}
+
+export default Profile;
