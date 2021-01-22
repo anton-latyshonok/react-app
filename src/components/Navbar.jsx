e7cedaaab95422bd2cea8c34ad2fc2be6a08ded5
Anton Latyshonok <antonlatyshonok@gmail.com>
2021-01-22 15:08:28 +0300

    added css modules

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import s from './Navbar.module.css';
+
+//let c1="item";
+//let c2 ="active";
+//"item active"
+//let classes = c1 + " " + c2;
+//let classesNew = `${s.item} ${s.active}`;
 
 const Navbar = () => {
-    return <nav className='nav'>
-    <div>
+    return <nav className={s.nav}>
+    <div className={s.item}>
       <a>Profile</a>
     </div>
-    <div>
+    <div className={`${s.item} ${s.active}`}>
       <a>Messages</a>
     </div>
-    <div>
+    <div className={s.item}>
       <a>News</a>
     </div>
-    <div>
+    <div className={s.item}>
       <a>Music</a>
     </div>
     </nav>;
