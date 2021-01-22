b69185af306ad12e210b9fdb114cefcbc560ffc5
Anton Latyshonok <antonlatyshonok@gmail.com>
2021-01-22 19:03:59 +0300

    add props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,10 +11,8 @@ const MyPosts = () => {
         <button>Remove</button>
       </div>
       <div className={s.posts}>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        <Post message='Hi, how are you?' />
+        <Post message="It's my first post" />
         <Post />
       </div>
     </div>);
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (<div>
         <div className={s.item}>
           <img src='https://upload.wikimedia.org/wikipedia/commons/7/77/Avatar_cat.png'></img>
-          post 1
+          {props.message}
           <div>
           <span>Like</span>
           </div>
