b1004c69bf2c6367d8333d19bb7eca553df90ccd
Anton Latyshonok <antonlatyshonok@gmail.com>
2021-01-23 01:43:16 +0300

    add route

--- src/App.js
+++ src/App.js
@@ -4,26 +4,33 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Header/Header.jsx';
 import Header from './components/Navbar/Navbar.jsx';
-//import Profile from './components/Profile/Profile.jsx';
-import Dialogs from "./components/Dialogs/Dialogs";
+import Profile from './components/Profile/Profile.jsx';
+import Dialogs from "./components/Dialogs/Dialogs.jsx";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import {BrowserRouter, Route} from "react-router-dom";
+
 
 
 const App = () => {
-  return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      {/*<Profile />*/}
-      <div className='app-wrapper-content'>
-      <Dialogs />
-      </div>
-      
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+
+
+                </div>
+
+            </div>
+        </BrowserRouter>
+    );
 }
-
-
-
 
 
 export default App;
