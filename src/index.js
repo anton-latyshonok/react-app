18c40e0f96e074198b531fd9cfc1acd98f868f08
Anton Latyshonok <antonlatyshonok@gmail.com>
2021-01-28 00:25:53 +0300

    context API

--- src/index.js
+++ src/index.js
@@ -5,26 +5,24 @@ import store from "./redux/redux-store";
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "./StoreContext";
 
-let renderEntireTree = (state) => {
+let renderEntireTree = () => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state}
-                 dispatch={store.dispatch.bind(store)}
-                 store={store}/>
+            <Provider store={store}>
+                <App />
+            </Provider>
         </BrowserRouter>,
         document.getElementById('root')
     )
 }
 
-renderEntireTree(store.getState());
+renderEntireTree();
 
 store.subscribe(() => {
-    let state = store.getState();
-    renderEntireTree(state);
+    renderEntireTree();
 });
-
-
 
 
 // If you want to start measuring performance in your app, pass a function
