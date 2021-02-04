715ae192ce4d6cc67e589dd6a88427c9a681c286
Anton Latyshonok <antonlatyshonok@gmail.com>
2021-02-04 13:42:26 +0300

    get users api

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,43 +1,27 @@
 import React from 'react';
 import styles from './Users.module.css';
+import axios from "axios";
+import userPhoto from '../../assets/images/logo.jpg';
+
 
 let Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    photoUrl: 'https://i.pinimg.com/originals/53/0e/93/530e9312f7e236423622a03a2ff259a5.jpg',
-                    followed: false,
-                    fullName: 'Sharlotta',
-                    status: 'I am a dog',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://i.pinimg.com/originals/53/0e/93/530e9312f7e236423622a03a2ff259a5.jpg',
-                    followed: true,
-                    fullName: 'Mavr',
-                    status: 'I am a dog',
-                    location: {city: 'Moscow', country: 'Russia'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://i.pinimg.com/originals/53/0e/93/530e9312f7e236423622a03a2ff259a5.jpg',
-                    followed: false,
-                    fullName: 'Endi',
-                    status: 'I am a dog',
-                    location: {city: 'Pinsk', country: 'Belarus'}
-                }
-            ]
-        )
+    let getUsers = () =>
+            {
+                if (props.users.length === 0) {
+                    axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                        .then(response => {
+                                props.setUsers(response.data.items);
+                            });
+               }
     }
 
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(u => <div key={u.id}>
                                 <span>
                     <div>
-                      <img src={u.photoUrl} className={styles.userPhoto}/>
+                      <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                     </div>
                     <div>
                       {u.followed
@@ -52,12 +36,12 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                    </span>
                 </span>
             </div>)
